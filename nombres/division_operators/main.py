ab5da2a498cf423ad0e20b05116d10f2fca7ce14
vincent-devFullStack <>
2026-01-03 15:14:37 +0000

    Division Operators

---
+++ nombres/division_operators/main.py
@@ -0,0 +1,8 @@
+# Calculate the number of completed transactions
+completed = 60 // 7
+# Calculate the number of remaining minutes
+minutes = 60 % completed
+
+# Print these values
+print("The number of completed transactions is", completed)
+print("The number of remaining minutes is", minutes)
