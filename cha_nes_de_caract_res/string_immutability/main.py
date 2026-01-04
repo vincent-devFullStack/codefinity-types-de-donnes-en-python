d7e9a6d08c01743678a54c528d5b18fafa1cd553
vincent-devFullStack <>
2026-01-04 17:49:48 +0000

    String Immutability

---
+++ cha_nes_de_caract_res/string_immutability/main.py
@@ -0,0 +1,12 @@
+string1 = "Rome is the capital of France"
+string2 = "Brasilia is located in Europe"
+string3 = "Monkeys eat earphones"
+
+# Replace substrings to correct the mistakes
+new_string1 = string1.replace('Rome', 'Paris')
+new_string2 = string2.replace('Europe', 'South America')
+new_string3 = string3.replace('earphones', 'bananas')
+
+print(new_string1)
+print(new_string2)
+print(new_string3)
