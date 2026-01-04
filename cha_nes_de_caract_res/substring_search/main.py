a1e6aae5f4d059c99f04c91bd606a5b100cae3b3
vincent-devFullStack <>
2026-01-04 09:34:07 +0000

    Substring search

---
+++ cha_nes_de_caract_res/substring_search/main.py
@@ -0,0 +1,14 @@
+string = "Work your way up!"
+
+# Find the index of the letter `p`
+p = string.find('p')
+
+# Find the index of the letter `y`
+y = string.find('y')
+
+# Find the index of the letter `r`
+r = string.find('r')
+
+print("Variable p equals:", p)
+print("Variable y equals:", y)
+print("Variable r equals:", r)
