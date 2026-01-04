101843136db15e84046e42df581011a9c54e85f6
vincent-devFullStack <>
2026-01-04 17:42:43 +0000

    index() Function

---
+++ cha_nes_de_caract_res/index_function/main.py
@@ -0,0 +1,14 @@
+string = "It is cool to have a burning desire for studying"
+
+# Find the index of "burning" word
+burning = string.index('burning')
+
+# Find the index of "desire" word
+desire = string.index('desire')
+
+# Find the index of "studying" word
+studying = string.index('studying')
+
+print("The index of the word burning is:", burning)
+print("The index of the word desire is:", desire)
+print("The index of the word studying is:", studying)
